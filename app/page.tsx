faefe77d64828dfdac13bf06a2d79b41f670c633
Max Tretikov <max@tretikov.com>
2024-06-15 16:55:19 -0600

    Remove navbar, move control panel to top right

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,16 @@
 import CanvasContextMenu from "@/components/canvas-context-menu";
 import ControlPanel from "@/components/control-panel";
 import FlowEditor from "@/components/flow-editor";
-import Navbar from "@/components/navbar";
 import { ReactFlowProvider } from "reactflow";
 
 export default function Home() {
   return (
     <div className="h-screen">
-      <Navbar />
       <ReactFlowProvider>
         <CanvasContextMenu>
           <FlowEditor />
         </CanvasContextMenu>
       </ReactFlowProvider>
-
       <ControlPanel />
     </div>
   );
