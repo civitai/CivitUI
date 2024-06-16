8e85741354ada4ce62a13fd6cdbb2198e1d85ff9
Max Tretikov <max@tretikov.com>
2024-06-16 02:51:08 -0600

    More sensible default workflow

--- app/defaultWorkflow.ts
+++ app/defaultWorkflow.ts
@@ -1,155 +1,192 @@
 import type { PersistedGraph } from "@/types";
 
 const defaultWorkflow: PersistedGraph = {
-  data: {
-    "d6488046-7e3d-45ce-b0cb-fc461ded790d": {
-      value: {
-        widget: "VAELoader",
-        fields: { vae_name: "vae-ft-mse-840000-ema-pruned.safetensors" },
+  "data": {
+    "1ab1ba4a-5dfb-4ced-bc06-2b55af930969": {
+      "value": {
+        "widget": "CheckpointLoaderSimple",
+        "fields": {
+          "ckpt_name": "Aniverse.safetensors"
+        }
       },
-      position: { x: 320, y: 1219.5 },
-      width: 319,
-      height: 120,
-    },
-    "196c7067-dd8c-4b21-95bf-c7da1074359f": {
-      value: { widget: "Group", fields: {}, modify: { nickname: "Prompt" } },
-      position: { x: 320.6155722066724, y: 1360.4647275134319 },
-      width: 319,
-      height: 539,
-    },
-    "2e19ea7c-a469-4aee-ba5f-50546f7b6856": {
-      value: {
-        widget: "PreviewImage",
-        fields: {},
-        images: [
-          { filename: "ComfyUI_00015_.png", subfolder: "", type: "temp" },
-        ],
+      "position": {
+        "x": -170.13106312032482,
+        "y": 1038.0670301674156
       },
-      position: { x: 959.9330329915367, y: 1040.5498613443042 },
-      width: 535,
-      height: 574,
-    },
-    "2d69027b-18cb-4fba-a6f7-3cc6191da27a": {
-      value: {
-        widget: "EmptyLatentImage",
-        fields: { width: 512, height: 512, batch_size: 1 },
-      },
-      position: { x: 680, y: 1040 },
-      width: 240,
-      height: 246,
-    },
-    "df10802c-dcb3-45cb-b61a-ed21c9435e54": {
-      value: { widget: "VAEDecode", fields: {} },
-      position: { x: 680, y: 1780 },
-      width: 240,
-      height: 96,
-    },
-    "3ab52db7-7835-409b-9742-cdbb84283789": {
-      value: {
-        widget: "KSampler",
-        fields: {
-          seed: 13316703440321622000,
-          steps: 24,
-          cfg: 8,
-          sampler_name: "dpm_2",
-          scheduler: "normal",
-          denoise: 0.99,
-        },
-      },
-      position: { x: 680, y: 1300 },
-      width: 240,
-      height: 461,
-    },
-    "03e009af-40ce-40f8-8678-ee811fe31f41": {
-      value: {
-        widget: "CLIPTextEncode",
-        fields: { text: "(worst quality:1.2), (low quality:1.2)" },
-        modify: { nickname: "Negative" },
-      },
-      position: { x: 19.384427793327575, y: 280.91726159315385 },
-      width: 280,
-      height: 228,
-      parentNode: "196c7067-dd8c-4b21-95bf-c7da1074359f",
+      "width": 317,
+      "height": 148
     },
     "18d73b78-7fd7-4411-a39e-b8a17e677892": {
-      value: {
-        widget: "CLIPTextEncode",
-        fields: {
-          text: "best quality,masterpiece,highres,original,extremely detailed wallpaper,perfect lighting,extremely detailed CG,blurry background, pbcmf,bodysuit,cleavage,pink hair,coif,bcmf,jacket,red hair,black coif",
+      "value": {
+        "widget": "CLIPTextEncode",
+        "fields": {
+          "text": "best quality,masterpiece,highres,original,extremely detailed wallpaper,perfect lighting,extremely detailed CG,blurry background, pbcmf,bodysuit,cleavage,pink hair,coif,bcmf,jacket,red hair,black coif"
         },
-        modify: { nickname: "Positive" },
+        "modify": {
+          "nickname": "Positive"
+        }
       },
-      position: { x: 20, y: 40.91726159315385 },
-      width: 278,
-      height: 154,
-      parentNode: "196c7067-dd8c-4b21-95bf-c7da1074359f",
-    },
-    "1ab1ba4a-5dfb-4ced-bc06-2b55af930969": {
-      value: {
-        widget: "CheckpointLoaderSimple",
-        fields: { ckpt_name: "Aniverse.safetensors" },
+      "position": {
+        "x": 351.937818230367,
+        "y": 913.007276869801
       },
-      position: { x: 321.49999999999994, y: 1041 },
-      width: 317,
-      height: 148,
+      "width": 278,
+      "height": 154
     },
+    "03e009af-40ce-40f8-8678-ee811fe31f41": {
+      "value": {
+        "widget": "CLIPTextEncode",
+        "fields": {
+          "text": "(worst quality:1.2), (low quality:1.2)"
+        },
+        "modify": {
+          "nickname": "Negative"
+        }
+      },
+      "position": {
+        "x": 356.16598581059907,
+        "y": 1169.2562517108377
+      },
+      "width": 280,
+      "height": 228
+    },
+    "3ab52db7-7835-409b-9742-cdbb84283789": {
+      "value": {
+        "widget": "KSampler",
+        "fields": {
+          "seed": 13316703440321622000,
+          "steps": 24,
+          "cfg": 8,
+          "sampler_name": "dpm_2",
+          "scheduler": "normal",
+          "denoise": 0.99
+        }
+      },
+      "position": {
+        "x": 852.5481575303295,
+        "y": 1036.0737133524628
+      },
+      "width": 240,
+      "height": 461
+    },
+    "df10802c-dcb3-45cb-b61a-ed21c9435e54": {
+      "value": {
+        "widget": "VAEDecode",
+        "fields": {}
+      },
+      "position": {
+        "x": 1193.2869359712133,
+        "y": 1125.515155272075
+      },
+      "width": 240,
+      "height": 96
+    },
+    "2d69027b-18cb-4fba-a6f7-3cc6191da27a": {
+      "value": {
+        "widget": "EmptyLatentImage",
+        "fields": {
+          "width": 512,
+          "height": 512,
+          "batch_size": 1
+        }
+      },
+      "position": {
+        "x": 382.3331953885338,
+        "y": 1427.6407120078889
+      },
+      "width": 240,
+      "height": 246
+    },
+    "2e19ea7c-a469-4aee-ba5f-50546f7b6856": {
+      "value": {
+        "widget": "PreviewImage",
+        "fields": {},
+        "images": [
+          {
+            "filename": "ComfyUI_00015_.png",
+            "subfolder": "",
+            "type": "temp"
+          }
+        ]
+      },
+      "position": {
+        "x": 1557.343054394227,
+        "y": 1189.245780369873
+      },
+      "width": 535,
+      "height": 574
+    },
+    "d6488046-7e3d-45ce-b0cb-fc461ded790d": {
+      "value": {
+        "widget": "VAELoader",
+        "fields": {
+          "vae_name": "vae-ft-mse-840000-ema-pruned.safetensors"
+        }
+      },
+      "position": {
+        "x": 733.4640796048611,
+        "y": 1581.809795858935
+      },
+      "width": 319,
+      "height": 120
+    }
   },
-  connections: [
+  "connections": [
     {
-      source: "1ab1ba4a-5dfb-4ced-bc06-2b55af930969",
-      sourceHandle: "MODEL",
-      target: "3ab52db7-7835-409b-9742-cdbb84283789",
-      targetHandle: "model",
+      "source": "1ab1ba4a-5dfb-4ced-bc06-2b55af930969",
+      "sourceHandle": "MODEL",
+      "target": "3ab52db7-7835-409b-9742-cdbb84283789",
+      "targetHandle": "model"
     },
     {
-      source: "1ab1ba4a-5dfb-4ced-bc06-2b55af930969",
-      sourceHandle: "CLIP",
-      target: "18d73b78-7fd7-4411-a39e-b8a17e677892",
-      targetHandle: "clip",
+      "source": "1ab1ba4a-5dfb-4ced-bc06-2b55af930969",
+      "sourceHandle": "CLIP",
+      "target": "18d73b78-7fd7-4411-a39e-b8a17e677892",
+      "targetHandle": "clip"
     },
     {
-      source: "1ab1ba4a-5dfb-4ced-bc06-2b55af930969",
-      sourceHandle: "CLIP",
-      target: "03e009af-40ce-40f8-8678-ee811fe31f41",
-      targetHandle: "clip",
+      "source": "1ab1ba4a-5dfb-4ced-bc06-2b55af930969",
+      "sourceHandle": "CLIP",
+      "target": "03e009af-40ce-40f8-8678-ee811fe31f41",
+      "targetHandle": "clip"
     },
     {
-      source: "03e009af-40ce-40f8-8678-ee811fe31f41",
-      sourceHandle: "CONDITIONING",
-      target: "3ab52db7-7835-409b-9742-cdbb84283789",
-      targetHandle: "negative",
+      "source": "03e009af-40ce-40f8-8678-ee811fe31f41",
+      "sourceHandle": "CONDITIONING",
+      "target": "3ab52db7-7835-409b-9742-cdbb84283789",
+      "targetHandle": "negative"
     },
     {
-      source: "18d73b78-7fd7-4411-a39e-b8a17e677892",
-      sourceHandle: "CONDITIONING",
-      target: "3ab52db7-7835-409b-9742-cdbb84283789",
-      targetHandle: "positive",
+      "source": "18d73b78-7fd7-4411-a39e-b8a17e677892",
+      "sourceHandle": "CONDITIONING",
+      "target": "3ab52db7-7835-409b-9742-cdbb84283789",
+      "targetHandle": "positive"
     },
     {
-      source: "2d69027b-18cb-4fba-a6f7-3cc6191da27a",
-      sourceHandle: "LATENT",
-      target: "3ab52db7-7835-409b-9742-cdbb84283789",
-      targetHandle: "latent_image",
+      "source": "2d69027b-18cb-4fba-a6f7-3cc6191da27a",
+      "sourceHandle": "LATENT",
+      "target": "3ab52db7-7835-409b-9742-cdbb84283789",
+      "targetHandle": "latent_image"
     },
     {
-      source: "3ab52db7-7835-409b-9742-cdbb84283789",
-      sourceHandle: "LATENT",
-      target: "df10802c-dcb3-45cb-b61a-ed21c9435e54",
-      targetHandle: "samples",
+      "source": "3ab52db7-7835-409b-9742-cdbb84283789",
+      "sourceHandle": "LATENT",
+      "target": "df10802c-dcb3-45cb-b61a-ed21c9435e54",
+      "targetHandle": "samples"
     },
     {
-      source: "df10802c-dcb3-45cb-b61a-ed21c9435e54",
-      sourceHandle: "IMAGE",
-      target: "2e19ea7c-a469-4aee-ba5f-50546f7b6856",
-      targetHandle: "images",
+      "source": "d6488046-7e3d-45ce-b0cb-fc461ded790d",
+      "sourceHandle": "VAE",
+      "target": "df10802c-dcb3-45cb-b61a-ed21c9435e54",
+      "targetHandle": "vae"
     },
     {
-      source: "d6488046-7e3d-45ce-b0cb-fc461ded790d",
-      sourceHandle: "VAE",
-      target: "df10802c-dcb3-45cb-b61a-ed21c9435e54",
-      targetHandle: "vae",
-    },
-  ],
+      "source": "df10802c-dcb3-45cb-b61a-ed21c9435e54",
+      "sourceHandle": "IMAGE",
+      "target": "2e19ea7c-a469-4aee-ba5f-50546f7b6856",
+      "targetHandle": "images"
+    }
+  ]
 };
 
 export default defaultWorkflow;
