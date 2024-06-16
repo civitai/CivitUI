0cffff432aef5058cfca9a4b841a17b649806e84
Max Tretikov <max@tretikov.com>
2024-06-15 21:43:24 -0600

    Add tooltips to control panel items

--- components/queue-prompt-button.tsx
+++ components/queue-prompt-button.tsx
@@ -4,19 +4,11 @@ import { PlayIcon } from "@radix-ui/react-icons";
 import { MultiStepLoader as Loader } from "@/components/ui/multi-step-loader";
 import { useCallback, useEffect, useState } from "react";
 import { Button } from "./ui/button";
+import { TooltipProvider, Tooltip, TooltipTrigger, TooltipContent } from "@/components/ui/tooltip";
 import { useAppStore } from "@/store";
 import { useShallow } from "zustand/react/shallow";
 import { toast } from "sonner";
 import { cn } from "@/lib/utils";
-
-const nodes = [
-  {
-    text: "CheckpointLoaderSimple",
-  },
-  {
-    text: "KSampler",
-  },
-];
 
 export const QueuePromptButton = () => {
   const [loading, setLoading] = useState(false);
@@ -52,20 +44,25 @@ export const QueuePromptButton = () => {
   const queueHasItems = queue.length > 0;
 
   return (
-    <>
+    <Tooltip>
       {/* TODO: Fix MultiStepLoader */}
-      <Loader loadingStates={nodes} loading={loading} />
+      {/* <Loader loadingStates={nodes} loading={loading} /> */}
 
-      <Button
-        className={cn(
-          "relative h-12 w-12 rounded-3xl shadow-lg bg-gradient-to-b text-white dark:text-black dark:from-white dark:to-blue-50 ring-2 ring-blue-50 ring-opacity-60",
-          "from-slate-800 to-slate-700 ring-slate-400",
-          "hover:rounded-lg transition-all duration-200"
-        )}
-        onClick={handleRun}
-      >
-        <PlayIcon />
-      </Button>
-    </>
+      <TooltipTrigger>
+        <Button
+          className={cn(
+            "relative h-12 w-12 rounded-3xl shadow-lg bg-gradient-to-b text-white dark:text-black dark:from-white dark:to-blue-50 ring-2 ring-blue-50 ring-opacity-60",
+            "from-slate-800 to-slate-700 ring-slate-400",
+            "hover:rounded-lg transition-all duration-200"
+          )}
+          onClick={handleRun}
+        >
+          <PlayIcon />
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent side="left" className="text-xs">
+        Queue prompt
+      </TooltipContent>
+    </Tooltip>
   );
 };
