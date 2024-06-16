ec42e8c27b8ea02082c29853b91f39eea7ca550f
Max Tretikov <max@tretikov.com>
2024-06-16 02:45:17 -0600

    Remove undo/redo from context menu

--- components/canvas-context-menu.tsx
+++ components/canvas-context-menu.tsx
@@ -12,37 +12,15 @@ import {
   ContextMenuSubTrigger,
   ContextMenuTrigger,
 } from "@/components/ui/context-menu";
-import useUndoRedo from "@/hooks/use-undo-redo";
-import { useAppStore } from "@/store";
-import { EdgeTypeList } from "@/types";
-import { useShallow } from "zustand/react/shallow";
 
 export default function CanvasContextMenu({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { undo, redo, canUndo, canRedo } = useUndoRedo();
-  const { edgeType, onEdgesType } = useAppStore(
-    useShallow((state) => ({
-      edgeType: state.edgeType,
-      onEdgesType: state.onEdgesType,
-    }))
-  );
-
   return (
     <ContextMenu>
       <ContextMenuTrigger>{children}</ContextMenuTrigger>
-      <ContextMenuContent className="w-64">
-        <ContextMenuItem inset onClick={undo} disabled={!canUndo}>
-          Undo
-          <ContextMenuShortcut>⌘Z</ContextMenuShortcut>
-        </ContextMenuItem>
-        <ContextMenuItem inset onClick={redo} disabled={!canRedo}>
-          Redo
-          <ContextMenuShortcut>⌘ShiftZ</ContextMenuShortcut>
-        </ContextMenuItem>
-      </ContextMenuContent>
     </ContextMenu>
   );
 }
