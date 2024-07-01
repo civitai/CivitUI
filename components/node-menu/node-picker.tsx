4f7a40591546e9f5618ad72098fa2adaaad2a3a0
Max Tretikov <max@tretikov.com>
2024-07-01 09:05:50 -0700

    Disable autocomplete in node search

--- components/node-menu/node-picker.tsx
+++ components/node-menu/node-picker.tsx
@@ -68,6 +68,7 @@ const NodePickerComponent = ({ setActiveItem }: any) => {
         <div className="relative w-full">
           <input
             name="search"
+            autoComplete="off"
             type="text"
             className="px-9 flex h-6 w-full rounded-md border border-input bg-transparent text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
             placeholder="Search..."
